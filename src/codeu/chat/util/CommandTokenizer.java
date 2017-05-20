9109b84e00665017e4afdcb470daf1e63895c846
TheMkrage <metoofarmer@yahoo.com>
2017-05-20 13:43:24 -0700

    Added CommandTokenizer Class with blank methods

---
+++ src/codeu/chat/util/CommandTokenizer.java
@@ -0,0 +1,28 @@
+package codeu.chat.util;
+
+public class CommandTokenizer {
+
+	private String source;
+	private int currentIndex = 0;
+
+	public CommandTokenizer(String source) {
+		// trims the source to rid leading and lagging white space
+		this.source = source.trim();
+	}
+	
+	public boolean hasNextArg() {
+		
+	}
+	
+	public String getArgs() {
+		
+	}
+
+	public String getNextArg() {
+		
+	}
+	
+	public String getCommand() {
+		
+	}
+}
