adfa596ae9cd1355c681e48d08be7f68af652dc5
TheMkrage <metoofarmer@yahoo.com>
2017-05-21 21:37:40 -0700

    filled in methods

--- src/codeu/chat/util/CommandTokenizer.java
+++ src/codeu/chat/util/CommandTokenizer.java
@@ -1,5 +1,8 @@
 package codeu.chat.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CommandTokenizer {
 
 	private String source;
@@ -8,21 +11,50 @@ public class CommandTokenizer {
 	public CommandTokenizer(String source) {
 		// trims the source to rid leading and lagging white space
 		this.source = source.trim();
+		// runs get Command once so currentIndex is ready to return args
+		getCommand();
 	}
-	
+
 	public boolean hasNextArg() {
-		
+		int remainingIndices = source.length() - currentIndex;
+		return remainingIndices > 0;
 	}
-	
-	public String getArgs() {
-		
+
+	public List<String> getRemainingArgs() {
+		ArrayList<String> args = new ArrayList<String>();
+		while (hasNextArg()) {
+			args.add(getNextArg());
+		}
+		return args;
 	}
 
 	public String getNextArg() {
-		
+		if (!this.hasNextArg()) {
+			return null;
+		}
+		while (source.charAt(currentIndex) == ' ') {
+			currentIndex++;
+		}
+		if (source.charAt(currentIndex) == '"') {
+			currentIndex++;
+			return nextStringUntil('"');
+		}
+		return nextStringUntil(' ');
 	}
-	
+
 	public String getCommand() {
-		
+		currentIndex = 0;
+		return getNextArg();
 	}
+
+	private String nextStringUntil(char endingChar) {
+		String toReturn = "";
+		while (hasNextArg() && source.charAt(currentIndex) != endingChar) {
+			toReturn += source.charAt(currentIndex);
+			currentIndex++;
+		}
+		currentIndex++;
+		return toReturn;
+	}
+
 }
