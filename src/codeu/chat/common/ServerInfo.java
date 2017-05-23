28957610159e4f9614586abc2c2cf37a0cb4b059
Priya411 <Priya411@users.noreply.github.com>
2017-05-23 11:12:51 -0700

    Added comments to ServerInfo.java

--- src/codeu/chat/common/ServerInfo.java
+++ src/codeu/chat/common/ServerInfo.java
@@ -1,9 +1,9 @@
-/*Class created by Priyanka Agarwal. It allows the version of the server
+/* Class created by Priyanka Agarwal. It allows the version of the server
 to be stored and updated. ServerInfo contains the variable Server_Version
 which represents the version, starting with 1.0.0. It can then be updated 
-as needed. The toString method allows the version to be converted from Uuid
-to a string so that it can be displayed in the Panels, to be use by the Chat.java
-class.
+as needed, whenever the server version changes. The toString method allows 
+the version to be converted from Uuid to a string so that it can be 
+displayed in the Panels, to be use by the Chat.java class.
  */
 
 package codeu.chat.common;
@@ -22,10 +22,14 @@ public final class ServerInfo {
             System.out.println("Invalid input");
         }
     }
+    
+    // This constructor allows for an updated version to be made
     public ServerInfo(Uuid version) {
         this.version = version;
     }
 
+    // This method allows the version to be printed as a string, 
+    // to help with displaying the information. 
     public String toString()
     {
         return version.toString();
