dcbf606b96b86968c854edb5fe06ab067121442a
TheMkrage <metoofarmer@yahoo.com>
2017-05-24 15:19:06 -0700

    Code Review Changes Implemented

--- src/codeu/chat/util/CommandTokenizer.java
+++ src/codeu/chat/util/CommandTokenizer.java
@@ -3,32 +3,27 @@ package codeu.chat.util;
 import java.util.ArrayList;
 import java.util.List;
 
-public class CommandTokenizer {
+final public class CommandTokenizer {
 
 	private String source;
 	private int currentIndex = 0;
-
-	public CommandTokenizer(String source) {
-		// trims the source to rid leading and lagging white space
-		this.source = source.trim();
-		// runs get Command once so currentIndex is ready to return args
-		// after running getCommand, currentIndex will be ready to tokenize arg0
-		getCommand();
-	}
 
 	public boolean hasNextArg() {
 		int remainingIndices = source.length() - currentIndex;
 		return remainingIndices > 0;
 	}
 
-	// returns all remaining args from the currentIndex to the end of the source
-	// string
-	public List<String> getRemainingArgs() {
-		ArrayList<String> args = new ArrayList<String>();
-		while (hasNextArg()) {
-			args.add(getNextArg());
+	// returns a String starting at currentIndex until the next occurrence of
+	// endingChar.
+	// Does not include endingChar in the String
+	private String nextStringUntil(char endingChar) {
+		String toReturn = "";
+		while (hasNextArg() && source.charAt(currentIndex) != endingChar) {
+			toReturn += source.charAt(currentIndex);
+			currentIndex++;
 		}
-		return args;
+		currentIndex++;
+		return toReturn;
 	}
 
 	public String getNextArg() {
@@ -47,21 +42,27 @@ public class CommandTokenizer {
 		return nextStringUntil(' ');
 	}
 
+	// resets currentIndex and returns Command Strings
 	public String getCommand() {
 		currentIndex = 0;
 		return getNextArg();
 	}
 
-	// returns a String starting at currentIndex until the next occurrence of
-	// endingChar.
-	// Does not include endingChar in the String
-	private String nextStringUntil(char endingChar) {
-		String toReturn = "";
-		while (hasNextArg() && source.charAt(currentIndex) != endingChar) {
-			toReturn += source.charAt(currentIndex);
-			currentIndex++;
+	public CommandTokenizer(String source) {
+		// trims the source to rid leading and lagging white space
+		this.source = source.trim();
+		// runs get Command once so currentIndex is ready to return args
+		// after running getCommand, currentIndex will be ready to tokenize arg0
+		getCommand();
+	}
+
+	// returns all remaining args from the currentIndex to the end of the source
+	// string
+	public List<String> getRemainingArgs() {
+		ArrayList<String> args = new ArrayList<String>();
+		while (hasNextArg()) {
+			args.add(getNextArg());
 		}
-		currentIndex++;
-		return toReturn;
+		return args;
 	}
 }
