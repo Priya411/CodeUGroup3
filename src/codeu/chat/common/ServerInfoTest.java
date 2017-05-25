6e9ba2e3a1910fb07b9063f6b869c2f6c5dc6f98
Priya411 <Priya411@users.noreply.github.com>
2017-05-25 13:15:13 -0700

    Separated and Updated tests

--- src/codeu/chat/common/ServerInfoTest.java
+++ src/codeu/chat/common/ServerInfoTest.java
@@ -7,25 +7,40 @@ import java.io.IOException;
 
 public class ServerInfoTest {
     boolean errorThrown = false;
+
     @Test
     public void testServerInfo() {
+        // Server version should just take out negative sign
+        // because it gets converted to unsigned, however
+        // an error is still thrown to alert user
         try{
-            // Server version should just take out negative sign
             ServerInfo server = new ServerInfo(Uuid.parse("-1.0.1"));
             assertEquals("1.0.1", server.toString());
         }
         catch (IOException e){
-            System.out.println("Invalid input");
+            errorThrown = true;
         }
+        assertTrue(errorThrown);
+        errorThrown = false;
+    }
+    
+    @Test
+    public void testServerInfo2() {
+        // This is the general case where no error should be thrown
+        // All versions will be in this format if inputted correctly
         try{
             ServerInfo server2 = new ServerInfo(Uuid.parse("1.0.1"));
             assertEquals("1.0.1", server2.toString());
         }
         catch (IOException e){
-            System.out.println("Invalid input");
+            fail();
         }
+    }
+
+    @Test
+    public void testServerInfoError() {
+        // Exception only caught when version is larger than 32 bits
         try {
-            // Exception only caught when version is larger than 32 bits
             ServerInfo server3 = new ServerInfo(Uuid.parse("1.999999999999999999"));
             fail(); // if we got here, no exception was thrown, which is bad
         }
@@ -34,10 +49,13 @@ public class ServerInfoTest {
         }
         assertTrue(errorThrown);
         errorThrown = false;
+    }
 
+    @Test
+    public void testServerInfoError2() {
+        // Exception only caught when version is larger than 32 bits
+        // Testing that it doesn't matter that the equal sign is there
         try {
-            // Exception only caught when version is larger than 32 bits
-            // Testing that it doesn't matter that the equal sign is there
             ServerInfo server4 = new ServerInfo(Uuid.parse("-1.999999999999999999"));
             fail(); // if we got here, no exception was thrown, which is bad
         }
