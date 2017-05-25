caca3bde4ac7f72630f8edaceb276e430141f876
Priya411 <Priya411@users.noreply.github.com>
2017-05-25 11:27:33 -0700

    Added three more tests

--- src/codeu/chat/common/ServerInfoTest.java
+++ src/codeu/chat/common/ServerInfoTest.java
@@ -1,5 +1,5 @@
 package codeu.chat.common;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 import org.junit.Test;
 import codeu.chat.util.Uuid;
 
@@ -10,11 +10,36 @@ public class ServerInfoTest {
     @Test
     public void testServerInfo() {
         try{
+            // Server version should just take out negative sign
+            ServerInfo server = new ServerInfo(Uuid.parse("-1.0.1"));
+            assertEquals("1.0.1", server.toString());
+        }
+        catch (IOException e){
+            System.out.println("Invalid input");
+        }
+        try{
             ServerInfo server2 = new ServerInfo(Uuid.parse("1.0.1"));
             assertEquals("1.0.1", server2.toString());
         }
         catch (IOException e){
             System.out.println("Invalid input");
+        }
+        try {
+            // Exception only caught when version is larger than 32 bits
+            ServerInfo server3 = new ServerInfo(Uuid.parse("1.999999999999999999"));
+            fail(); // if we got here, no exception was thrown, which is bad
+        }
+        catch (IOException e) {
+            assertEquals(true,true);
+        }
+        try {
+            // Exception only caught when version is larger than 32 bits
+            // Testing that it doesn't matter that the equal sign is there
+            ServerInfo server4 = new ServerInfo(Uuid.parse("-1.999999999999999999"));
+            fail(); // if we got here, no exception was thrown, which is bad
+        }
+        catch (IOException e) {
+            assertEquals(true,true);
         }
     }
 }
