cf0620e2bcc47c2568afc6b52d2077af9ffc1a1c
TheMkrage <metoofarmer@yahoo.com>
2017-05-26 13:03:26 -0700

    Added Many more tests

--- test/codeu/chat/util/CommandTokenizerTest.java
+++ test/codeu/chat/util/CommandTokenizerTest.java
@@ -20,5 +20,32 @@ public class CommandTokenizerTest {
 		assertEquals("arg0", spaceTokenizer.getNextArg());
 		assertEquals("arg1", spaceTokenizer.getNextArg());
 		assertEquals(2, spaceTokenizer.getRemainingArgs().size());
+		
+		CommandTokenizer noSpaceTokenizerWithQuotes = new CommandTokenizer(
+				"Command \"arg0\"\"arg1\"\"arg2\"\"arg3\"");
+		assertEquals("Command", noSpaceTokenizerWithQuotes.getCommand());
+		assertEquals("arg0", noSpaceTokenizerWithQuotes.getNextArg());
+		assertEquals("arg1", noSpaceTokenizerWithQuotes.getNextArg());
+		assertEquals(2, noSpaceTokenizerWithQuotes.getRemainingArgs().size());
+		
+		CommandTokenizer spacesWithinQuotesTokenizer = new CommandTokenizer(
+				"Command \"arg0 \"\"arg 1\"\"arg2\"\"arg3\"");
+		assertEquals("Command", spacesWithinQuotesTokenizer.getCommand());
+		assertEquals("arg0 ", spacesWithinQuotesTokenizer.getNextArg());
+		assertEquals("arg 1", spacesWithinQuotesTokenizer.getNextArg());
+		assertEquals(2, spacesWithinQuotesTokenizer.getRemainingArgs().size());
+		
+		CommandTokenizer alternatingTokenizer = new CommandTokenizer(
+				"Command \"arg0 \" arg1 \"arg2\" arg3");
+		assertEquals("Command", alternatingTokenizer.getCommand());
+		assertEquals("arg0 ", alternatingTokenizer.getNextArg());
+		assertEquals("arg1", alternatingTokenizer.getNextArg());
+		assertEquals("arg2", alternatingTokenizer.getNextArg());
+		assertEquals("arg3", alternatingTokenizer.getNextArg());
+		
+		CommandTokenizer blankTokenizer = new CommandTokenizer(
+				"Command");
+		assertEquals(0, blankTokenizer.getRemainingArgs().size());
+		assertEquals(null, blankTokenizer.getNextArg());
 	}
 }
