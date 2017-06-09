a7a6f06d5b542fc9840069f80a22ff8ae6746a43
TheMkrage <metoofarmer@yahoo.com>
2017-06-08 21:27:02 -0700

    JSONReader with save methods

---
+++ src/codeu/chat/server/JSONReader.java
@@ -0,0 +1,32 @@
+package codeu.chat.server;
+
+import codeu.chat.common.ConversationHeader;
+import codeu.chat.common.Message;
+import codeu.chat.common.User;
+
+public final class JSONReader {
+
+	/**
+	 * Appends a user to the json file
+	 * @param user
+	 */
+	public static void save(User user) {
+		
+	}
+	
+	/**
+	 * Appends a message to the json file
+	 * @param user
+	 */
+	public static void save(Message message) {
+		
+	}
+
+	/**
+	 * Appends a conversation to the json file
+	 * @param user
+	 */
+	public static void save(ConversationHeader conversation) {
+		
+	}
+}
