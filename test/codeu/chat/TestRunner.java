3282e80087f23ecee44698854581442d335e2181
Priyanka Agarwal <pragarwal.agarwal@gmail.com>
2017-06-15 19:53:20 -0700

    Changed order to remove conflicts

--- test/codeu/chat/TestRunner.java
+++ test/codeu/chat/TestRunner.java
@@ -27,13 +27,12 @@ public final class TestRunner {
 						codeu.chat.relay.ServerTest.class,
 						codeu.chat.server.BasicControllerTest.class,
 						codeu.chat.server.RawControllerTest.class,
-						codeu.chat.server.JSONTest.class,
 						codeu.chat.util.TimeTest.class,
 						codeu.chat.util.UuidTest.class,
 						codeu.chat.util.store.StoreTest.class,
 						codeu.chat.util.UpTimeTest.class,
-			            codeu.chat.common.ServerInfoTest.class
-
+			            codeu.chat.common.ServerInfoTest.class,
+						codeu.chat.server.JSONTest.class
 						);
 		for (final Failure failure : result.getFailures()) {
 			System.out.println(failure.toString());
