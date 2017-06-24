f7133f85464c7af9ebed319f1801977fcff40341
julezjw <juliajwang396@yahoo.com>
2017-06-24 14:14:02 -0700

    Update User.java
    added function to show userInterest updates

--- src/codeu/chat/common/User.java
+++ src/codeu/chat/common/User.java
@@ -17,7 +17,13 @@ package codeu.chat.common;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.UUID;
 
+import codeu.chat.client.core.ConversationContext;
+import codeu.chat.client.core.MessageContext;
 import codeu.chat.util.Serializer;
 import codeu.chat.util.Serializers;
 import codeu.chat.util.Time;
@@ -61,6 +67,13 @@ public final class User {
   public final String name;
   @JsonIgnore
   public final Time creation;
+  
+  
+  // keeps track of user's USER interests 
+  // ASSUMPTION getters/setters will be added by Priyanka 
+  public ArrayList<Uuid> userInterests = new ArrayList<Uuid>(); 
+  // time of user's last update recorded as a long 
+  public long lastUpdateTime; 
 
   public User(Uuid id, String name, Time creation) {
     this.id = id;
@@ -75,6 +88,7 @@ public final class User {
   public long getCreationTime() {
 	  return creation.inMs();
   }
+
 
   @Override
   public boolean equals (Object toCompare)
@@ -103,5 +117,61 @@ public final class User {
     hash+=user.name.hashCode();
     hash+=user.creation.hashCode();
     return hash;
+  }
+  
+  // USERstatusUpdate()
+  // 
+  // Returns all the user updates in an dictionary 
+  // where keys are the user's user interests (UUIDs) 
+  // and a value is an arraylist of arraylists<String> 
+  // where the first arraylist keeps track of each userinterest's 
+  // newly created conversations and the second arraylist keeps 
+  // track of the conversations the userinterest has contributed messages to 
+  //
+  public HashMap<Uuid, ArrayList<ArrayList<String>>> USERstatusUpdate(Iterable<ConversationContext> conversations){ 
+	  HashMap<Uuid, ArrayList<ArrayList<String>>> userUpdates = new HashMap<Uuid, ArrayList<ArrayList<String>>>(); 
+
+	  // ASSUMPTION: saving user interests by UUID 
+	  // ASSUMPTION: all of the names saved in userInterests are indeed 
+	  //			 valid users in the system 
+	  for (Uuid uuid : this.userInterests){
+		  // every user gets added the to dictionary with
+		  // KEY = (String) userName 
+		  // VALUE = [ [(Strings) conversationsCreatedNames], [(Strings) conversationsContributedNames] ] 
+		  userUpdates.put(uuid, new ArrayList<ArrayList<String>>(Arrays.asList(
+				  new ArrayList<String>() {}, new ArrayList<String>() {}))); 
+	  }
+	  
+	  for (ConversationContext convo : conversations){
+		  MessageContext lastmessage = convo.lastMessage(); 
+		  
+		  if (lastmessage.message.getCreationTime() < this.lastUpdateTime){ 
+			  // if the last message in the conversation was sent before lastUpdateTime 
+			  // that means it was covered in the last status update and we can move on 
+			  continue;
+		  }
+		  
+		  if (convo.conversation.creation.inMs() > this.lastUpdateTime && 
+				  this.userInterests.contains(convo.conversation.owner.id())){ 
+			  // if the convo was created by a user of interest, it will be added 
+			  // to that user's value's first arraylist 
+			  userUpdates.get(convo.conversation.owner.id()).get(0).add(convo.conversation.title); 
+		  }
+		  
+		  while (lastmessage != null && lastmessage.message.getCreationTime() < this.lastUpdateTime) { 
+			  // moving backwards in the message log until the message sentTimes 
+			  // become smaller than LastUpdateTime conversationsCreated[]
+			  if (this.userInterests.contains(lastmessage.message.author) && 
+					  !userUpdates.get(convo.conversation.owner.id()).get(1).contains(convo.conversation.title)){
+				  // if the message was created by a user of interst, it will be added 
+				  // to that user's value's second arraylist conversationsContributed[]
+				  // Also checks to make sure that conversation hasn't already been added 
+				  userUpdates.get(convo.conversation.owner.id()).get(1).add(convo.conversation.title);
+			  }
+			  lastmessage = lastmessage.previous(); 
+		  }
+		  
+	  }
+	  return userUpdates; 
   }
 }
