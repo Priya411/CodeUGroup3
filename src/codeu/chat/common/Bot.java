a630b93e6dc9780e5345d6cfdb6e3ff02da4728f
Priyanka Agarwal <pragarwal.agarwal@gmail.com>
2017-08-01 10:22:36 -0700

    Change ReactTo signature

--- src/codeu/chat/common/Bot.java
+++ src/codeu/chat/common/Bot.java
@@ -1,8 +1,10 @@
 package codeu.chat.common;
 
+import codeu.chat.util.Uuid;
+
 public abstract class Bot {
 
-    abstract String reactTo(String command, String[] args);
+    abstract String reactTo(String text, Uuid sender);
     abstract String onAdd();  // what bot does when it is first added
     public String getName()
     {
